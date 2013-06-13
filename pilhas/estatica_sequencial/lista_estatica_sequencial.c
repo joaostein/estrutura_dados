cd0280c6b7b99ce76c0b98498d3b03cf3bc8f211
Joao Stein <joaosteing@gmail.com>
2013-06-13 12:01:42 -0300

    Added create function for Pilha

--- pilhas/estatica_sequencial/lista_estatica_sequencial.c
+++ pilhas/estatica_sequencial/lista_estatica_sequencial.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM 10
+
+
+typedef struct Pilha {
+    int dado[TAM];
+    int topo;
+} Pilha;
+
+// Create
+
+Pilha *create () {
+    Pilha *p = malloc(sizeof(Pilha));
+    p->topo = -1;
+    return p;
+}
+
+// Push
+
+// Seek
+
+// Pop
+
+// isEmpty?
+
 
 int main () {
-    printf("teste\n");
+    Pilha *p = create();
+
+    printf("topo: %d\n", p->topo);
 
 
 
