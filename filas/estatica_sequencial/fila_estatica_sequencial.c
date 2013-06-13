c0d1283d96f5ac80c6118a14814811b9e4b24464
Joao Stein <joaosteing@gmail.com>
2013-06-13 16:25:45 -0300

    Added function to remove the first item on Fila

--- filas/estatica_sequencial/fila_estatica_sequencial.c
+++ filas/estatica_sequencial/fila_estatica_sequencial.c
@@ -5,7 +5,7 @@
 
 typedef struct Fila {
     int elemento[TAMANHO];
-    int inicio, tamanho;
+    int inicio, quantidadeElementos;
 } Fila;
 
 // Create (Cria a fila)
@@ -13,15 +13,33 @@ typedef struct Fila {
 Fila *create () {
     Fila *f = malloc(sizeof(Fila));
     f->inicio = 0;
-    f->tamanho = 0;
+    f->quantidadeElementos = 0;
 
     return f;
 }
 
-
 // Push (Adiciona um elemento ao final da fila - FIFO [First in, First Out])
+void push (Fila *f, int elemento) {
+    if (f->quantidadeElementos == TAMANHO) {
+        printf("A fila está cheia. Não suporta novos elementos.\n");
+    } else {
+        f->elemento[(f->inicio + f->quantidadeElementos) % TAMANHO] = elemento;
+        f->quantidadeElementos ++;
+    }
+}
 
 // Pop (Remove o elemento que se encontra no início da fila)
+int pop (Fila *f) {
+    if (f->quantidadeElementos == 0) {
+        printf("Impossível remover elementos. A fila está vazia.\n");
+    } else {
+        int elemento = f->elemento[f->inicio];
+        f->inicio = (f->inicio + 1) % TAMANHO;
+        f->quantidadeElementos --;
+
+        return elemento;
+    }
+}
 
 // Seek (Retorna o valor que está no início da fila)
 
@@ -31,6 +49,11 @@ Fila *create () {
 int main () {
 
     Fila *f = create();
+    
+    push(f, 182);
+    printf("Elemento[0]: %d\n", f->elemento[0]);
+
+    printf("Removido elemento (%d).\n", pop(f));
 
     return 0;
 }
