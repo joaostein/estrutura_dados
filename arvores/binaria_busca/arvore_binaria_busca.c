3462703036f1be60ba5b332ce05248dd1db41b92
Joao Stein <joaosteing@gmail.com>
2013-06-14 16:29:05 -0300

    Added search function

--- arvores/binaria_busca/arvore_binaria_busca.c
+++ arvores/binaria_busca/arvore_binaria_busca.c
@@ -33,21 +33,54 @@ void push (No **raiz, int elemento) {
     if (elemento > (*raiz)->dado) {
         push(&((*raiz)->dir), elemento);
     }
-    
-
-
 }
 
-
-// Pop
-
 // Search
+
+No *search (No *raiz, int elemento) {
+    // Lista vazia
+    if (raiz == NULL ) {
+        printf("Não foi possível encontrar elemento.\n");
+        return NULL;
+    }
+
+    // Árvore populada. Apenas existe um elemento que é a raiz.
+    if (elemento == raiz->dado) {
+        return raiz;
+    }
+
+    // Árvore populada. Elemento a ser buscado menor que raiz.
+    if (elemento < raiz->dado) {
+        return search(raiz->esq, elemento);
+    }
+
+    // Árvore populada. Elemento a ser buscado menor que raiz.
+    if (elemento > raiz->dado) {
+        return search(raiz->dir, elemento);
+    }
+}
+
+// Remove
+
 
 
 int main () {
 
     No *raiz = create();
+    push(&raiz, 5);
+    push(&raiz, 3);
+    push(&raiz, 2);
+    push(&raiz, 7);
+    push(&raiz, 6);
+    push(&raiz, 8);
     push(&raiz, 10);
+
+    pop(&raiz, )
+
+    No *elemento = search(raiz, 6);
+    if (elemento != NULL) {
+        printf("Elemento buscado: %d\n", elemento->dado);
+    }
 
     return 0;
 }
