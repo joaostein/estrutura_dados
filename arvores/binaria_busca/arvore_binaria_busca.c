3f182c2e7ad67761017393f7e1e5ddc466f798eb
Joao Stein <joaosteing@gmail.com>
2013-06-14 16:00:14 -0300

    Added push function to arvore

--- arvores/binaria_busca/arvore_binaria_busca.c
+++ arvores/binaria_busca/arvore_binaria_busca.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct No {
+    struct No *esq;
+    struct No *dir;
+    int dado;
+} No;
+
+// Create
+
+No *create () {
+    return NULL;
+}
+
+// Push
+
+void push (No **raiz, int elemento) {
+    // Lista vazia
+    if (*raiz == NULL) {
+        *raiz = malloc(sizeof(No));
+        (*raiz)->dado = elemento;
+        (*raiz)->esq = NULL;
+        (*raiz)->dir = NULL;
+    }
+
+    // Árvore populada. Elemento a ser inserido menor que raiz.
+    if (elemento < (*raiz)->dado) {
+        push(&((*raiz)->esq), elemento);
+    }
+
+    //  Árvore populada. Elemento a ser inserido mairo que raiz.
+    if (elemento > (*raiz)->dado) {
+        push(&((*raiz)->dir), elemento);
+    }
+    
+
+
+}
+
+
+// Pop
+
+// Search
+
+
 int main () {
-    printf("Teste.\n");
+
+    No *raiz = create();
+    push(&raiz, 10);
 
     return 0;
 }
